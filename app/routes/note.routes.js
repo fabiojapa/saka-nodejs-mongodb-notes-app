cd7d5eef335e2318183c38fda85ec133561ff0e3
Fabio Sakamoto <fabiojapa@gmail.com>
2020-04-07 17:04:55 -0300

    swagger

--- app/routes/note.routes.js
+++ app/routes/note.routes.js
@@ -1,18 +1,27 @@
 module.exports = (app) => {
+    const express = require('express'),
+    router = express.Router(),
+    swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('../../config/swagger.json');
+  
+
     const notes = require('../controllers/note.controller.js');
 
     // Create a new Note
-    app.post('/notes', notes.create);
+    router.post('/notes', notes.create);
 
     // Retrieve all Notes
-    app.get('/notes', notes.findAll);
+    router.get('/notes', notes.findAll);
 
     // Retrieve a single Note with noteId
-    app.get('/notes/:noteId', notes.findOne);
+    router.get('/notes/:noteId', notes.findOne);
 
     // Update a Note with noteId
-    app.put('/notes/:noteId', notes.update);
+    router.put('/notes/:noteId', notes.update);
 
     // Delete a Note with noteId
-    app.delete('/notes/:noteId', notes.delete);
+    router.delete('/notes/:noteId', notes.delete);
+
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    app.use('/api/v1', router);
 }
