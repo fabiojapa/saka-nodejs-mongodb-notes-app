51357992742fc8a714cd27142fe6679af7de3c9e
Fabio Sakamoto <fabiojapa@gmail.com>
2020-04-06 21:08:09 -0300

    database local

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // url: 'mongodb://root:tivit123@mongo:27017/easy?authSource=admin'
-    url: 'mongodb://heroku_zdd147sk:odlc1b4e7erc74rf2gnf6i5999@ds059115.mlab.com:59115/heroku_zdd147sk'
+    url: 'mongodb://root:tivit123@mongo:27017/easy?authSource=admin'
+    // url: 'mongodb://heroku_zdd147sk:odlc1b4e7erc74rf2gnf6i5999@ds059115.mlab.com:59115/heroku_zdd147sk'
 }
